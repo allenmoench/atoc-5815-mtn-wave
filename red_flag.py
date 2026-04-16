555325eaa543cab0919d20a50ceaae9fd86810ac
allenmoench <moenchallen@gmail.com>
2026-04-16 10:52:25 -0600

    first commit

---
+++ red_flag.py
@@ -0,0 +1,7 @@
+"""This file will look for thresholds in the data, to identify red flag days"""
+
+def identify_red_flag_days(start_date, end_date):
+    """identifies red flag days, as defined by certain thresholds"""
+    
+    rf_days_list = []
+
